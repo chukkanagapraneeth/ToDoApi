88a0e437eefa62aa36c889431512d720f392a168
Shin <cnagapraneeth@gmail.com>
2025-01-02 20:41:40 +0530

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApi.DTOs;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        public static List<string> _todos = new List<string>(); 
+
+        [HttpGet("/GetTodos")]
+        public IActionResult GetTodos()
+        {
+            return Ok(_todos);
+        }
+
+        [HttpPost("/CreateTodo")]
+        public IActionResult CreateTodo([FromBody] string msg)
+        {
+            _todos.Add(msg);
+            return Ok("Added");
+        }
+
+        [HttpPut("/UpdateTodo")]
+        public IActionResult UpdateTodo([FromBody] UpdateDTO update)
+        {
+            _todos[update.Id] = update.msg;
+            return Ok(_todos);
+        }
+
+        [HttpDelete("/DeleteTodo")]
+        public IActionResult DeleteTodo(int id)
+        {
+            _todos.RemoveAt(id);
+            return Ok("Removed");
+        }
+    }
+}
---
+++ DTOs/UpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoApi.DTOs
+{
+    public class UpdateDTO
+    {
+        public int Id { get; set; }
+        public string msg { get; set; }
+    }
+}
